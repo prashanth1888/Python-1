012e003f5646a5b851d7854ef80b1f2fdea12199
arunbaindla <arunkumar.888.b@gmail.com>
2025-07-07 02:20:21 +0530

    Update Python_practise.py

--- Python_practise.py
+++ Python_practise.py
@@ -15,3 +15,54 @@ elif(num>2):
         print("prime number")
 
 --------------------------------------------------------
+Fibonacci series 
+--------------------------------------------------------
+num=int(input())
+f1=0
+f2=1
+print(f1)
+print(f2)
+c=0
+for i in range(0,num+1):
+    c=f1+f2
+    print(c)
+    f1=f2
+    f2=c
+--------------------------------------------------------
+Armstrong number 
+--------------------------------------------------------
+num=input("enter the number to check")
+sum=0
+result=int(num)
+for i in range(0,len(num)):
+    a=int(num[i])
+    sum=a*a*a+sum
+    
+print(sum)
+if(sum==result):
+    print(num+"is Armstrong num")
+    
+-------------------------------------------------
+Is it Palindrome or not 
+-------------------------------------------------
+str=input("enter the strig to check palindrome")
+rev=str[::-1]
+if(str==rev):
+ print("is palindrome")
+else:
+    print("not palindrome ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+----------------------------------------------------------
