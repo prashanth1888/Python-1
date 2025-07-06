59c73b455d6992c7485bdf46389561517b908742
arunbaindla <arunkumar.888.b@gmail.com>
2025-07-07 03:35:24 +0530

    Create Reverse string.py

---
+++ programs/Reverse string.py
@@ -0,0 +1,11 @@
+#Functions - Reverse of a string
+	def reverse(s):
+	    x=s[::-1]
+	    return x
+print(reverse("ravi"))
+--------------------------------------
+
+str=input("enter the string")
+rev=str[::-1]
+print(rev)
+
