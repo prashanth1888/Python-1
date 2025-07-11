8711af2a8c39aa98130585a17aa4d4fb5d6a9364
arunbaindla <arunkumar.888.b@gmail.com>
2025-07-12 01:26:48 +0530

    Update prime number.py

--- programs/prime number.py
+++ programs/prime number.py
@@ -1,17 +1,14 @@
 Check if the number is prime or not 
 ------------------------------------------------
-num=int(input("enter numbers greater than 1"))
+ n=int(input("enter a number : "))
 count=0
-if(num==2):
-    print("is prime number")
-elif(num>2):
-    for i in range(1,num-1):
-        if(num%(num-i)==0):
-            count=count+1
-    if(count>0):
-        print("it is not a prime num")
-    else:
-        print("prime number")
+for i in range(2,n):
+    if(n%i==0):
+        count=count+1
+if(count>=1):
+    print("not prime ", n)
+else:
+    print("prime")
 
 ----------------------------------------------------------
 Prime numbers in the given range 
